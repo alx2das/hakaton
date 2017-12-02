ecca4cd6690f00db099901647ae2c74bd4bbb610
alexandr.buylov <buylovav@modulbank.ru>
2017-12-02 17:18:40 +0300

    _

--- source/js/components/AppContainer.js
+++ source/js/components/AppContainer.js
@@ -6,23 +6,25 @@ import {Switch} from 'react-router-dom'
 
 @withRouter
 export default class AppContainer extends React.Component {
-	static propTypes = {
-		routes: PropTypes.array.isRequired
-	};
+    static propTypes = {
+        routes: PropTypes.array.isRequired
+    };
 
-	render() {
-		const {routes} = this.props;
-		return (
-			<Switch>
-				{routes.map((p, index) => (
-					<Route
-						key={index}
-						exact={p.exact}
-						path={p.path}
-						component={p.component}
-					/>
-				))}
-			</Switch>
-		);
-	}
+    render() {
+        const {routes} = this.props;
+        return (
+            <Route path={'/hakaton/build/'}>
+                <Switch>
+                    {routes.map((p, index) => (
+                        <Route
+                            key={index}
+                            exact={p.exact}
+                            path={p.path}
+                            component={p.component}
+                        />
+                    ))}
+                </Switch>
+            </Route>
+        );
+    }
 }
