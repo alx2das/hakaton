d5264a2d155c10baee5661c62d2698846c8e2352
alexandr.buylov <buylovav@modulbank.ru>
2017-12-03 04:43:50 +0300

    _

--- source/js/modules/hunterSale/containers/OptionsContainer.js
+++ source/js/modules/hunterSale/containers/OptionsContainer.js
@@ -3,27 +3,50 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import toJS from 'components/HOC/toJs'
 import {Link} from 'react-router-dom'
+import {Select} from 'common/uiElements'
 
 
 function mapStateToProps(state, ownProps) {
-	return {}
+    return {}
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({}, dispatch);
+    return bindActionCreators({}, dispatch);
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 @toJS
 export default class extends Component {
-	render() {
-		console.log('fullState', this.props);
-		return (
-			<div>
-                <h1>OptionsContainer</h1>
-				<Link to={'/'}>Go Statistic</Link>
-			</div>
-		)
+    render() {
+        console.log('fullState', this.props);
+        return (
+            <div>
 
-	}
+                <div className="title_panel">
+                    <Link to={'/options'} className="button small light orange f_right">К статистике</Link>
+                </div>
+
+                <div className="tabs_flat tabs_flat__h1">
+                    <a className="tab tab__active">Охота за продажами</a>
+                    <a className="tab">Темная карма</a>
+                </div>
+
+                <h3 className='opt_title'>План на неделю</h3>
+
+                <div className='opt_box'>
+                    <div className='opt_label'>Точка продаж</div>
+                    <Select
+                        className='w30'
+                        options={[
+                            {label: 'Первый нах'},
+                            {label: 'Второй епт.'},
+                            {label: 'Последний бля'}
+                        ]}
+                    />
+                </div>
+
+            </div>
+        )
+
+    }
 }
